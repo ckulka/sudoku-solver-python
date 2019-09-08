60d1fee3a3f08c18b245400d57eb591b09ded0fd
ckulka <ckulka@wlgore.com>
2019-09-08 16:47:57 +0200

    Added file parser for sudokus

--- sudoku_solver/sudoku_file_reader.py
+++ sudoku_solver/sudoku_file_reader.py
@@ -1,9 +1,8 @@
-
 def parse(filename):
-    return parse_for_each_loop(filename)
+    return parse_with_lambdas(filename)
 
 
-def parse_for_each_loop(filename):
+def parse_with_for_loop(filename):
     sudoku = []
 
     file = open(filename)
@@ -18,5 +17,18 @@ def parse_for_each_loop(filename):
             else:
                 sudoku_line.append(int(field.strip()))
         sudoku.append(sudoku_line)
+
+    return sudoku
+
+
+def parse_with_lambdas(filename):
+    sudoku = []
+
+    file = open(filename)
+    for line in file.readlines():
+        fields = line.split(",")
+        fields = map(lambda text: text.strip(), fields)
+        fields = map(lambda text: None if text == "" else int(text), fields)
+        sudoku.append(list(fields))
 
     return sudoku
--- test/test_sudoku_file_reader.py
+++ test/test_sudoku_file_reader.py
@@ -1,7 +1,7 @@
 from sudoku_solver import sudoku_file_reader, sudoku_verifier
 
 
-def test_parse_for_each_loop():
+def test_parse_with_for_loop():
     expected = [
         [9, 8, 6, 7, 2, 3, 4, 5, 1],
         [3, 4, 1, 5, 6, 8, 2, 7, 9],
@@ -14,7 +14,28 @@ def test_parse_for_each_loop():
         [5, 7, 4, 9, 1, 6, 8, 2, None]
     ]
 
-    actual = sudoku_file_reader.parse("test/unsolved_sudokus/easy.txt")
+    input_file = "test/unsolved_sudokus/easy.txt"
+    actual = sudoku_file_reader.parse_with_for_loop(input_file)
+    # sudoku_verifier.print_sudoku(actual)
+
+    assert expected == actual
+
+
+def test_parse_with_lambdas():
+    expected = [
+        [9, 8, 6, 7, 2, 3, 4, 5, 1],
+        [3, 4, 1, 5, 6, 8, 2, 7, 9],
+        [2, 5, 7, 1, 4, 9, 6, 3, 8],
+        [8, 3, 5, 6, 9, 2, 1, 4, 7],
+        [4, 6, 9, 3, 7, 1, 5, 8, 2],
+        [7, 1, 2, 8, 5, 4, 3, 9, 6],
+        [1, 9, 3, 2, 8, 5, 7, 6, 4],
+        [6, 2, 8, 4, 3, 7, 9, 1, 5],
+        [5, 7, 4, 9, 1, 6, 8, 2, None]
+    ]
+
+    input_file = "test/unsolved_sudokus/easy.txt"
+    actual = sudoku_file_reader.parse_with_lambdas(input_file)
     # sudoku_verifier.print_sudoku(actual)
 
     assert expected == actual
